8557d4ba073aa58ccb4b11294a018b37e67eb5bc
Aditya Kumawat <adityakumawat502@gmail.com>
2022-12-25 14:27:20 +0530

    Added functionality to add images to blogs

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Category
+from blog.models import Post, Category, Image
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,5 +8,9 @@ class PostAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["title", "photo"]
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Image, ImageAdmin)
--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,7 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+class Image(models.Model):
+    title = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='pics')
