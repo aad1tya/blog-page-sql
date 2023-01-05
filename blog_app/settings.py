17448ab3e0d7dabf473e69120e9e6040fdbc82e6
Aditya Kumawat <adityakumawat502@gmail.com>
2023-01-05 07:34:16 +0530

    Trying to post it on github

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p-d1)luk-b+^m%*ipf2l10n&jph4=j!zl6h$(5uptq3d)19t99'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
